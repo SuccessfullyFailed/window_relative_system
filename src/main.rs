df726b8365728cce48927af553ae0a85510239ce
SuccessfullyFailed <renzogerritzen@gmail.com>
2025-04-28 15:05:44 +0200

    Project initialization and window-change hook

---
+++ src/main.rs
@@ -0,0 +1,7 @@
+use window_relative_system::window_hook;
+
+
+
+pub fn main() {
+	window_hook::install(false);
+}
---
+++ src/window_hook.rs
@@ -0,0 +1,48 @@
+use winapi::um::winuser::{DispatchMessageW, GetMessageW, SetWinEventHook, TranslateMessage, EVENT_SYSTEM_FOREGROUND, MSG, WINEVENT_OUTOFCONTEXT};
+use winapi::shared::{ minwindef::DWORD, ntdef::LONG, windef::{ HWINEVENTHOOK, HWINEVENTHOOK__, HWND } };
+use std::{ mem, ptr::null_mut, sync::{ Mutex, MutexGuard }, thread };
+
+
+
+static HOOK_INSTALLED:Mutex<bool> = Mutex::new(false);
+
+
+
+/// Install the window hook.
+pub fn install(create_thread:bool) {
+	if create_thread {
+		thread::spawn(|| install(false));
+	} else {
+		unsafe {
+		
+			// Validate no existing hook.
+			let mut hook_installed:MutexGuard<'_, bool> = HOOK_INSTALLED.lock().unwrap();
+			if *hook_installed {
+				eprintln!("Hook already in place.");
+				return;
+			}
+
+			// Create and validate hook.
+			let hook:*mut HWINEVENTHOOK__ = SetWinEventHook(EVENT_SYSTEM_FOREGROUND, EVENT_SYSTEM_FOREGROUND, null_mut(), Some(win_event_proc), 0, 0, WINEVENT_OUTOFCONTEXT);
+			if hook.is_null() {
+				eprintln!("Failed to set event hook.");
+				return;
+			}
+			*hook_installed = true;
+
+			// Keep listening for messages on hook.
+			let mut msg:MSG = mem::zeroed();
+			while GetMessageW(&mut msg, null_mut(), 0, 0) > 0 {
+				TranslateMessage(&msg);
+				DispatchMessageW(&msg);
+			}
+		}
+	}
+}
+
+/// Handle a windows hook event to process changes in active window.
+unsafe extern "system" fn win_event_proc(_event_hook:HWINEVENTHOOK, event:DWORD, _hwnd:HWND, _id_object:LONG, _id_child:LONG, _dw_event_thread:DWORD, _dwms_event_time:DWORD) {
+	if event == EVENT_SYSTEM_FOREGROUND {
+		println!("changed window");
+	}
+}
