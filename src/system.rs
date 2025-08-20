9376c65af7c1c9a6c96285380a23029372a31810
SuccessfullyFailed <renzogerritzen@gmail.com>
2025-08-20 17:58:47 +0200

    Move system locking to separate function, guaranteeing consistent system locking

--- src/system.rs
+++ src/system.rs
@@ -34,42 +34,32 @@ impl WindowRelativeSystem {
 
 	/// Update the current profile.
 	pub fn update_profile(current_window:WindowController) {
+		Self::execute_on_system(|system| {
 
-		// Get lock on system.
-		let mut system_guard:MutexGuard<'_, Option<WindowRelativeSystem>> = SYSTEM_INST.lock().unwrap();
-		if system_guard.is_none() {
-			*system_guard = Some(WindowRelativeSystem::default());
-		}
-		let system:&mut WindowRelativeSystem = (*system_guard).as_mut().unwrap();
+			// Find active profile.
+			let active_process_name:String = current_window.process_name().unwrap_or_default();
+			let active_process_title:String = current_window.title();
+			let active_profile_index:usize = system.profiles.iter().position(|profile| profile.is_active(&active_process_name, &active_process_title)).unwrap_or(0);
 
-		// If no profiles exist, DefaultProfile hasn't initialized, abort further actions.
-		if system.profiles.is_empty() {
-			return;
-		}
-
-		// Find active profile.
-		let active_process_name:String = current_window.process_name().unwrap_or_default();
-		let active_process_title:String = current_window.title();
-		let active_profile_index:usize = system.profiles.iter().position(|profile| profile.is_active(&active_process_name, &active_process_title)).unwrap_or(0);
-
-		// Profile change.
-		if active_profile_index != system.active_profile_index {
-			if let Err(error) = system.profiles[system.active_profile_index].trigger_deactivate_event() {
-				(system.error_handler)(&system.profiles[system.active_profile_index], "activate", &*error);
+			// Profile change.
+			if active_profile_index != system.active_profile_index {
+				if let Err(error) = system.profiles[system.active_profile_index].trigger_deactivate_event() {
+					(system.error_handler)(&system.profiles[system.active_profile_index], "activate", &*error);
+				}
+				system.active_profile_index = active_profile_index;
+				if let Err(error) = system.profiles[system.active_profile_index].trigger_activate_event() {
+					(system.error_handler)(&system.profiles[system.active_profile_index], "deactivate", &*error);
+				}
 			}
-			system.active_profile_index = active_profile_index;
-			if let Err(error) = system.profiles[system.active_profile_index].trigger_activate_event() {
-				(system.error_handler)(&system.profiles[system.active_profile_index], "deactivate", &*error);
-			}
-		}
+		});
 	}
 
 
 
 	/* USAGE METHODS */
 
-	/// Execute an action on the current profile.
-	pub fn execute_on_current_profile<T>(action:T) where T:Fn(&WindowRelativeProfile) {
+	/// Execute an action on a locked system instance.
+	pub(crate) fn execute_on_system<T>(action:T) where T:Fn(&mut WindowRelativeSystem) {
 
 		// Get lock on system.
 		let mut system_guard:MutexGuard<'_, Option<WindowRelativeSystem>> = SYSTEM_INST.lock().unwrap();
@@ -83,8 +73,15 @@ impl WindowRelativeSystem {
 			return;
 		}
 
-		// Execute action on current profile.
-		action(&system.profiles[system.active_profile_index]);
+		// Execute the action on the system.
+		action(system)
+	}
+
+	/// Execute an action on the current profile.
+	pub fn execute_on_current_profile<T>(action:T) where T:Fn(&WindowRelativeProfile) {
+		Self::execute_on_system(|system|
+			action(&system.profiles[system.active_profile_index])
+		);
 	}
 }
 impl Default for WindowRelativeSystem {
