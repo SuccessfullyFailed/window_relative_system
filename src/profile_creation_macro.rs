484299b4ea166dc9fab46b39e85f7437a05c6ed8
SuccessfullyFailed <renzogerritzen@gmail.com>
2025-10-20 02:30:49 +0200

    Allow no or multiple modifier functions for profile-creator macro

--- src/profile_creation_macro.rs
+++ src/profile_creation_macro.rs
@@ -1,11 +1,35 @@
 #[macro_export]
 macro_rules! create_profile {
-	($id:expr, $title:expr, $process_name:expr, $properties_modifier:expr) => {
+
+	// No modifiers
+	($id:expr, $title:expr, $process_name:expr) => {
+		#[ctor::ctor]
+		fn add_to_system() {
+			let profile = window_relative_system::WindowRelativeProfile::new($id, $title, $process_name);
+			window_relative_system::WindowRelativeSystem::add_profile(profile);
+		}
+	};
+
+	// One modifier
+	($id:expr, $title:expr, $process_name:expr, $modifier:expr) => {
 		#[ctor::ctor]
 		fn add_to_system() {
 			let mut profile = window_relative_system::WindowRelativeProfile::new($id, $title, $process_name);
-			let modifier:fn(window_relative_system::WindowRelativeProfile) -> window_relative_system::WindowRelativeProfile = $properties_modifier;
+			let modifier: fn(window_relative_system::WindowRelativeProfile) -> window_relative_system::WindowRelativeProfile = $modifier;
 			profile = modifier(profile);
+			window_relative_system::WindowRelativeSystem::add_profile(profile);
+		}
+	};
+
+	// Multiple modifiers (comma-separated list)
+	($id:expr, $title:expr, $process_name:expr, [$($modifier:expr),+ $(,)?]) => {
+		#[ctor::ctor]
+		fn add_to_system() {
+			let mut profile = window_relative_system::WindowRelativeProfile::new($id, $title, $process_name);
+			$(
+				let modifier: fn(window_relative_system::WindowRelativeProfile) -> window_relative_system::WindowRelativeProfile = $modifier;
+				profile = modifier(profile);
+			)+
 			window_relative_system::WindowRelativeSystem::add_profile(profile);
 		}
 	};
