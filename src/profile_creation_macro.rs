b88706cf3edbcf574d3d51bdd70f7ff9856878fb
SuccessfullyFailed <renzogerritzen@gmail.com>
2025-11-09 02:14:25 +0100

    Update profile creation macro

--- src/profile_creation_macro.rs
+++ src/profile_creation_macro.rs
@@ -3,9 +3,9 @@ macro_rules! create_profile {
 	($id:expr, $title:expr, $process_name:expr, $($modifier:expr),*) => {
 		#[ctor::ctor]
 		fn add_to_system() {
-			let mut profile = window_relative_system::WindowRelativeProfile::new($id, $title, $process_name);
+			let mut profile = window_relative_system::WindowRelativeProfileCore::new($id, $title, $process_name);
 			$(
-				let modifier:fn(window_relative_system::WindowRelativeProfile) -> window_relative_system::WindowRelativeProfile = $modifier;
+				let modifier:fn(window_relative_system::WindowRelativeProfileCore) -> window_relative_system::WindowRelativeProfileCore = $modifier;
 				profile = modifier(profile);
 			)+
 			window_relative_system::WindowRelativeSystem::add_profile(profile);
